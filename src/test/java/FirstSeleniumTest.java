167233c757250764815f627dbd405fb6fc0b605e
Kristina Muntian <kristinaflorentina01@gmail.com>
2023-01-24 10:41:48 +0100

    Added locator

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -14,6 +14,7 @@ public class FirstSeleniumTest {
     By passwordField = By.cssSelector("[placeholder=\"Password\"]");
 
     By confirmPassField = By.cssSelector("[placeholder=\"Confirm Password\"]");
+    By confirmPassField1 = By.cssSelector("[placeholder=\"Confirm Password\"]");
 
 
     //before
