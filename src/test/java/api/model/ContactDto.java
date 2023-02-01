8c1d3d303036091c0740b5f2dd6b5f1f9a802b05
Kristina Muntian <kristinaflorentina01@gmail.com>
2023-02-01 15:16:15 +0100

    Added API tests

---
+++ src/test/java/api/model/ContactDto.java
@@ -0,0 +1,18 @@
+package api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+//описывает тело нашего запроса
+public class ContactDto {
+    String firstName;
+    String lastName;
+    String description;
+
+}
