a3e656d4fb214c856c24930eab34e077ab64aa42
Kristina Muntian <kristinaflorentina01@gmail.com>
2023-02-03 11:56:01 +0100

    Added Login Class

---
+++ src/test/java/Login.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+
+public class Login extends TestBase {
+
+    // Login отдельно, потомучто его будем использовать всегда, кроме Create User
+
+    By loginForm = By.id("login-form");
+    By emailField = By.cssSelector("[placeholder=\"Email\"]");
+    By passwordField = By.cssSelector("[placeholder=\"Password\"]");
+    By loginButton = By.xpath("//*[@type=\"submit\"]");
+
+    By contactsTable = By.id("contacts-list");
+
+    @BeforeMethod
+    public void login() {
+        String userEmail = "test@gmail.com";
+        String password = "test@gmail.com";
+
+        Assert.assertTrue(isElementPresent(loginForm));
+        fillField(userEmail, emailField);
+        fillField(password, passwordField);
+        driver.findElement(loginButton).click();
+
+        Assert.assertTrue(isElementPresent(contactsTable));// если прыйдет исключение, то прийдет false
+    }
+}
+
